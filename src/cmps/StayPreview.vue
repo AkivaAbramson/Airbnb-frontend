e3bcb948a32f7c01f83ef0159c8718d70bc90c4f
Akiva Abramson <akivaabramson@gmail.com>
2023-07-28 00:28:13 +0300

    add users/sign in/login

--- src/cmps/StayPreview.vue
+++ src/cmps/StayPreview.vue
@@ -1,9 +1,7 @@
 <template>
-    <li class="stay-preview" v-for="imgUrl in stay.imgUrls" :key="imgUrl">
-        <!-- <RouterLink  :to="'/stay/' + stay._id"> -->
+    <li class="stay-preview" >
             <div class="prev-img-container">
                 <MainGallery :stay="stay" @goToDetails="$router.push('/stay/' + stay._id)"/>
-                <!-- <img class="preview-img" :src="imgUrl" /> -->
                 <div class="heart-icon">
                     <svg @click.prevent="toggleHeart" :class="{ liked: isLiked }" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" aria-hidden="true" role="presentation" focusable="false" style="display: block; height: 24px; width: 24px; stroke: white; stroke-width: 2; overflow: visible;">
                         <path d="M16 28c7-4.73 14-10 14-17a6.98 6.98 0 0 0-7-7c-1.8 0-3.58.68-4.95 2.05L16 8.1l-2.05-2.05a6.98 6.98 0 0 0-9.9 0A6.98 6.98 0 0 0 2 11c0 7 7 12.27 14 17z"></path>
