58af96c9c20a9a202db05cd15b94567a0d3035be
MorAlmog <gm.bebel12@gmail.com>
2023-07-28 14:15:43 +0300

    sync

--- src/cmps/AppHeader.vue
+++ src/cmps/AppHeader.vue
@@ -15,7 +15,7 @@
         <span>|</span>
         <span>Add guests</span>
         <span class="search-icon">
-          <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><!--<style>svg{fill:#ffffff}</style>--><path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z"/></svg>
+          <!-- <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><style>svg{fill:#ffffff}</style><path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z"/></svg> -->
         </span>
       </section>
   
@@ -23,8 +23,8 @@
         <RouterLink to="/stay/edit/">Airanbnb your home</RouterLink>
         <RouterLink to="/login">
           <div class="profile-menu">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" aria-hidden="true" role="presentation" focusable="false" style="height: 16px; width: 16px; stroke: #222222; stroke-width: 3; overflow: visible;"><g fill="none"><path d="M2 16h28M2 24h28M2 8h28"></path></g></svg>
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" aria-hidden="true" role="presentation" focusable="false" height="30px" width="30px" style="fill: #717171;"><path d="M16 .7C7.56.7.7 7.56.7 16S7.56 31.3 16 31.3 31.3 24.44 31.3 16 24.44.7 16 .7zm0 28c-4.02 0-7.6-1.88-9.93-4.81a12.43 12.43 0 0 1 6.45-4.4A6.5 6.5 0 0 1 9.5 14a6.5 6.5 0 0 1 13 0 6.51 6.51 0 0 1-3.02 5.5 12.42 12.42 0 0 1 6.45 4.4A12.67 12.67 0 0 1 16 28.7z"></path></svg>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" style="height: 16px; width: 16px; stroke: #222222;"><path d="M2 16h28M2 24h28M2 8h28"></path></svg>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" height="30px" width="30px" style="fill: #717171;"><path d="M16 .7C7.56.7.7 7.56.7 16S7.56 31.3 16 31.3 31.3 24.44 31.3 16 24.44.7 16 .7zm0 28c-4.02 0-7.6-1.88-9.93-4.81a12.43 12.43 0 0 1 6.45-4.4A6.5 6.5 0 0 1 9.5 14a6.5 6.5 0 0 1 13 0 6.51 6.51 0 0 1-3.02 5.5 12.42 12.42 0 0 1 6.45 4.4A12.67 12.67 0 0 1 16 28.7z"></path></svg>
           </div>
         </RouterLink>
       </nav>
--- src/cmps/AppHeaderFilter.vue
+++ src/cmps/AppHeaderFilter.vue
@@ -1,5 +1,8 @@
 <template>
     <section class="header-filter main-layout flex">
+        <section>
+
+        </section>
         <ul class="main-labels clean-list">
             <li class="label" v-for="label in labels" :key="labels">
                 <button class="label-button">
@@ -11,7 +14,7 @@
         <div class="main-filters">
             <button class="filter-button">
                 <div class="filter-icon">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" aria-hidden="true" role="presentation" focusable="false" style="display: block; height: 14px; width: 14px; fill: currentcolor;"><path d="M5 8a3 3 0 0 1 2.83 2H14v2H7.83A3 3 0 1 1 5 8zm0 2a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm6-8a3 3 0 1 1-2.83 4H2V4h6.17A3 3 0 0 1 11 2zm0 2a1 1 0 1 0 0 2 1 1 0 0 0 0-2z"></path></svg>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" style="height: 14px; width: 14px; fill: currentcolor;"><path d="M5 8a3 3 0 0 1 2.83 2H14v2H7.83A3 3 0 1 1 5 8zm0 2a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm6-8a3 3 0 1 1-2.83 4H2V4h6.17A3 3 0 0 1 11 2zm0 2a1 1 0 1 0 0 2 1 1 0 0 0 0-2z"></path></svg>
                 </div>
                 <p>Filters</p>
             </button>
