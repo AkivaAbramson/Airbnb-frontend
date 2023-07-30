742db297b4f0077db658a08da6a17eb93e6aca19
Akiva Abramson <akivaabramson@gmail.com>
2023-07-30 17:38:38 +0300

    login/signup modal complete

--- src/cmps/NavModal.vue
+++ src/cmps/NavModal.vue
@@ -1,30 +1,159 @@
   
 <!-- NavModal.vue -->
 <template>
-    <div class="nav-modal" v-show="showModal" @click.stop>
-        <!-- Add your navigation links and actions here -->
-        <!-- For example: -->
-        <RouterLink to="/login">Sign Up</RouterLink>
-        <!-- <a href="#">Sign Up</a> -->
-        <a href="#">Trips</a>
-        <a href="#">Wishlist</a>
-        <a @click="doLogout">Logout</a>
+    <div>
+        <div class="nav-modal" @click.stop>
+            <!-- <RouterLink to="/login">Sign Up</RouterLink> -->
+            
+            <a href="#" v-if="!this.loggedinUser" @click="showSignupModal">Sign Up</a>
+            <a href="#" v-if="!this.loggedinUser" @click="showLoginModal">Login</a>
+            <a href="#">Trips</a>
+            <a href="#">Wishlist</a>
+            <hr>
+            <RouterLink to="/stay/edit/">Airanbnb your home</RouterLink>
+            <a class="logout-btn" v-if="this.loggedinUser" @click="doLogout">Logout</a>
+        </div>
+
+        <div v-if="showSignup" @click.stop>
+            <div class="modal-container">
+                <div class="modal-content">
+                    <form @submit.prevent="doSignup">
+                        <section class="modal-header">
+                            <svg @click="closeModal" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" aria-hidden="true" role="presentation" focusable="false" style="display: block; fill: none; height: 16px; width: 16px; stroke: currentcolor; stroke-width: 3; overflow: visible;"><path d="m6 6 20 20M26 6 6 26"></path></svg>
+                            <h3 class="header-txt">Sign up</h3>
+                        </section>
+                        <hr>
+                        <h3>Welcome to Airbnb</h3>
+                        <input type="text" v-model="signupCred.fullname" placeholder="Your full name" />
+                        <input type="text" v-model="signupCred.username" placeholder="Username" />
+                        <input type="password" v-model="signupCred.password" placeholder="Password" />
+                        <ImgUploader @uploaded="onUploaded" />
+                        <div class="btn-wrapper">
+                            <FancyBtn class="submit-btn" :content="'Sign up'" />
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+
+        <div v-if="showLogin" @click.stop>
+            <div class="modal-container">
+                <div class="modal-content">
+                    <form @submit.prevent="doLogin">
+                        <section class="modal-header">
+                            <svg @click="closeModal" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" aria-hidden="true" role="presentation" focusable="false" style="display: block; fill: none; height: 16px; width: 16px; stroke: currentcolor; stroke-width: 3; overflow: visible;"><path d="m6 6 20 20M26 6 6 26"></path></svg>
+                            <h3 class="header-txt">Log in</h3>
+                        </section>
+                        <input type="text" v-model="loginCred.username" placeholder="Username" />
+                        <input type="password" v-model="loginCred.password" placeholder="Password" />
+                        <div class="btn-wrapper">
+                            <FancyBtn class="submit-btn" :content="'Login'" />
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+
+
     </div>
 </template>
   
   
 <script>
+import ImgUploader from './ImgUploader.vue'
+import FancyBtn from './FancyBtn.vue'
 export default {
     props: {
         showModal: Boolean,
     },
+    data() {
+        return {
+            // showModal: Boolean,
+            showSignup: false,
+            showLogin: false,
+            msg: '',
+            loginCred: { username: 'user1', password: '123' },
+            signupCred: { username: '', password: '', fullname: '', imgUrl: '' },
+        }
+    },
+    computed: {
+        users() {
+            return this.$store.getters.users
+        },
+        isLoading() {
+            return this.$store.getters.usersIsLoading
+        },
+        loggedinUser() {
+            return this.$store.getters.loggedinUser
+        },
+    },
+    created() {
+        this.loadUsers()
+    },
     methods: {
+        async doLogin() {
+            if (!this.loginCred.username) {
+                this.msg = 'Please enter username/password'
+                return
+            }
+            try {
+                await this.$store.dispatch({ type: "login", userCred: this.loginCred })
+                this.showLogin = !this.showLogin
+                this.$router.push('/')
+            } catch (err) {
+                console.log(err)
+                this.msg = 'Failed to login'
+            }
+        },
         doLogout() {
             this.$store.dispatch({ type: 'logout' })
-            
+        },
+        async doSignup() {
+            if (!this.signupCred.fullname || !this.signupCred.password || !this.signupCred.username) {
+                this.msg = 'Please fill up the form'
+                this.showModal = false
+                return
+            }
+            await this.$store.dispatch({ type: 'signup', userCred: this.signupCred })
+            this.showSignup = false
+            this.$router.push('/')
+
+        },
+        loadUsers() {
+            this.$store.dispatch({ type: "loadUsers" })
+        },
+        async removeUser(userId) {
+            try {
+                await this.$store.dispatch({ type: "removeUser", userId })
+                this.msg = 'User removed'
+            } catch (err) {
+                this.msg = 'Failed to remove user'
+            }
+        },
+        onUploaded(imgUrl) {
+            this.signupCred.imgUrl = imgUrl
         },
 
+        showSignupModal() {
+            this.showSignup = !this.showSignup
+        },
+        showLoginModal() {
+            this.showLogin = !this.showLogin
+        },
+        closeModal() {
+            this.showSignup = false
+            this.showLogin = false
+        },
+
+
     },
-};
+    components: {
+        ImgUploader,
+        FancyBtn
+
+    }
+}
 </script>
+
+
   
--- src/cmps/StayPreview.vue
+++ src/cmps/StayPreview.vue
@@ -33,7 +33,7 @@
                         <path fill-rule="evenodd" d="m15.1 1.58-4.13 8.88-9.86 1.27a1 1 0 0 0-.54 1.74l7.3 6.57-1.97 9.85a1 1 0 0 0 1.48 1.06l8.62-5 8.63 5a1 1 0 0 0 1.48-1.06l-1.97-9.85 7.3-6.57a1 1 0 0 0-.55-1.73l-9.86-1.28-4.12-8.88a1 1 0 0 0-1.82 0z"></path>
                     </svg> 
                 </div>
-                <h3 class="rate-num">{{ stay.reviews[0].rate }}.0</h3>
+                <h3 class="rate-num">{{ stay.reviews[0].rate.Value }}.0</h3>
             </section>
             
         </div>
