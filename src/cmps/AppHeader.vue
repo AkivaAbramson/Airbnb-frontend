594ed4e839295a5525b0b2e980c180a445bf859e
Akiva Abramson <akivaabramson@gmail.com>
2023-07-31 00:26:21 +0300

    search filter pixelPerfect

--- src/cmps/AppHeader.vue
+++ src/cmps/AppHeader.vue
@@ -1,5 +1,4 @@
 <template>
-
   <section class="home-layout">
     <header class="small-header">
       <nav>
@@ -7,38 +6,31 @@
           <span role="img" aria-label="logo">airanbnb</span>
         </RouterLink>
       </nav>
-  
+
       <section class="search-container">
-        <span>Anywhere</span>
-        <span>|</span>
-        <span>Any week</span>
-        <span>|</span>
-        <span>Add guests</span>
+        <span @click="locModal">Anywhere</span>
+        <span class="search-divider"></span>
+        <span @click="weekModal">Any week</span>
+        <span class="search-divider"></span>
+        <span @click="guestsModal" class="light-search-font">Add guests</span>
         <span class="search-icon">
-          <!-- <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><style>svg{fill:#ffffff}</style><path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z"/></svg> -->
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" aria-hidden="true" role="presentation"
+            focusable="false"
+            style="fill: none; height: 20px; width: 20px; stroke: white; stroke-width: 5.33333; overflow: visible;">
+            <path fill="none" d="M13 24a11 11 0 1 0 0-22 11 11 0 0 0 0 22zm8-3 9 9"></path>
+          </svg>
         </span>
       </section>
-  
+
+        <FilterModal v-if="location"/>
+        <FilterModal v-if="week"/>
+        <FilterModal v-if="guests"/>
+
       <nav>
         <RouterLink to="/stay/edit/">Airanbnb your home</RouterLink>
-        <!-- <RouterLink to="/login">
-          <div class="profile-menu">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" aria-hidden="true" role="presentation" focusable="false" style="height: 16px; width: 16px; stroke: #222222; stroke-width: 3; overflow: visible;"><g fill="none"><path d="M2 16h28M2 24h28M2 8h28"></path></g></svg>
-            <section class="loggedin-user" v-if="loggedInUser">
-              <img :src="loggedInUser.imgUrl" />
-            </section>
-            <svg class="profile-picture" v-else xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" aria-hidden="true" role="presentation" focusable="false" height="30px" width="30px" style="fill: #717171;"><path d="M16 .7C7.56.7.7 7.56.7 16S7.56 31.3 16 31.3 31.3 24.44 31.3 16 24.44.7 16 .7zm0 28c-4.02 0-7.6-1.88-9.93-4.81a12.43 12.43 0 0 1 6.45-4.4A6.5 6.5 0 0 1 9.5 14a6.5 6.5 0 0 1 13 0 6.51 6.51 0 0 1-3.02 5.5 12.42 12.42 0 0 1 6.45 4.4A12.67 12.67 0 0 1 16 28.7z"></path></svg>
-          </div>
-        </RouterLink> -->
         <div class="profile-menu" @click="toggleModal">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 32 32"
-            aria-hidden="true"
-            role="presentation"
-            focusable="false"
-            style="height: 16px; width: 16px; stroke: #222222; stroke-width: 3; overflow: visible;"
-          >
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" aria-hidden="true" role="presentation"
+            focusable="false" style="height: 16px; width: 16px; stroke: #222222; stroke-width: 3; overflow: visible;">
             <g fill="none">
               <path d="M2 16h28M2 24h28M2 8h28"></path>
             </g>
@@ -46,28 +38,37 @@
           <section class="loggedin-user" v-if="loggedInUser">
             <img :src="loggedInUser.imgUrl" />
           </section>
-          <svg class="profile-picture" v-else xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" aria-hidden="true" role="presentation" focusable="false" height="30px" width="30px" style="fill: #717171;"><path d="M16 .7C7.56.7.7 7.56.7 16S7.56 31.3 16 31.3 31.3 24.44 31.3 16 24.44.7 16 .7zm0 28c-4.02 0-7.6-1.88-9.93-4.81a12.43 12.43 0 0 1 6.45-4.4A6.5 6.5 0 0 1 9.5 14a6.5 6.5 0 0 1 13 0 6.51 6.51 0 0 1-3.02 5.5 12.42 12.42 0 0 1 6.45 4.4A12.67 12.67 0 0 1 16 28.7z"></path></svg>
+          <svg class="profile-picture" v-else xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" aria-hidden="true"
+            role="presentation" focusable="false" height="30px" width="30px" style="fill: #717171;">
+            <path
+              d="M16 .7C7.56.7.7 7.56.7 16S7.56 31.3 16 31.3 31.3 24.44 31.3 16 24.44.7 16 .7zm0 28c-4.02 0-7.6-1.88-9.93-4.81a12.43 12.43 0 0 1 6.45-4.4A6.5 6.5 0 0 1 9.5 14a6.5 6.5 0 0 1 13 0 6.51 6.51 0 0 1-3.02 5.5 12.42 12.42 0 0 1 6.45 4.4A12.67 12.67 0 0 1 16 28.7z">
+            </path>
+          </svg>
           <NavModal v-if="modalVisible" />
         </div>
 
       </nav>
-  
+
     </header>
     <AppHeaderFilter class="home-layout"></AppHeaderFilter>
-  
+
   </section>
-  
 </template>
 
 <script>
 import AppHeaderFilter from './AppHeaderFilter.vue'
 import NavModal from './NavModal.vue'
+import FilterModal from './FilterModal.vue'
 export default {
   name: 'header-app',
+
   data() {
 
     return {
-      modalVisible: false
+      modalVisible: false,
+      location: false,
+      week: false,
+      guests: false,
 
     }
 
@@ -82,12 +83,59 @@ export default {
   },
   methods: {
     toggleModal() {
-      this.modalVisible = !this.modalVisible;
+      this.modalVisible = !this.modalVisible
     },
+    locModal(){
+      this.location = true
+      this.week = false
+      this.guests = false
+      console.log(this.location)
+    },
+    weekModal(){
+      this.week = true
+      this.location = false
+      this.guests = false
+      console.log(this.week)
+    },
+    guestsModal(){
+      this.guests = true
+      this.location = false
+      this.week = false
+      console.log(this.guests)
+    }
   },
   components: {
     AppHeaderFilter,
-    NavModal
+    NavModal,
+    FilterModal,
   }
+
+  // setup() {
+  //   // Data
+  //   const showModal = ref({
+  //     location: false,
+  //     week: false,
+  //     guests: false,
+  //   });
+
+  //   // Methods
+  //   const openModal = (modalName) => {
+  //     showModal.value[modalName] = true;
+  //   };
+
+  //   const closeModal = (modalName) => {
+  //     showModal.value[modalName] = false;
+  //   };
+
+  //   // Computed
+  //   // You can add computed properties here if needed
+
+  //   return {
+  //     showModal,
+  //     openModal,
+  //     closeModal,
+  //   }
+  // },
+
 }
 </script>
--- src/services/stay.service.local.js
+++ src/services/stay.service.local.js
@@ -99,104 +99,104 @@ function getEmptyStay() {
 
 
 // Initial data
-// ; (async () => {
-//   await storageService.post(STORAGE_KEY, {
-//     "_id": "45478gj487dh",
-//     "name": "Ribeira Charming Duplex",
-//     "type": "House",
-//     "imgUrls": ["https://a0.muscache.com/im/pictures/e83e702f-ef49-40fb-8fa0-6512d7e26e9b.jpg?aki_policy=large",
-//       "https://a0.muscache.com/im/pictures/e83e702f-ef49-40fb-8fa0-6512d7e26e9b.jpg?aki_policy=large",
-//       "https://a0.muscache.com/im/pictures/e83e702f-ef49-40fb-8fa0-6512d7e26e9b.jpg?aki_policy=large",
-//       "https://a0.muscache.com/im/pictures/e83e702f-ef49-40fb-8fa0-6512d7e26e9b.jpg?aki_policy=large",
-//       "https://a0.muscache.com/im/pictures/e83e702f-ef49-40fb-8fa0-6512d7e26e9b.jpg?aki_policy=large",
-//       "https://a0.muscache.com/im/pictures/e83e702f-ef49-40fb-8fa0-6512d7e26e9b.jpg?aki_policy=large"],
-//     "price": 80.00,
-//     "summary": "Fantastic duplex apartment with three bedrooms, located in the historic area of Porto, Ribeira (Cube)...",
-//     "capacity": 8,
-//     "amenities": [
-//       "TV",
-//       "Wifi",
-//       "Kitchen",
-//       "Smoking allowed",
-//       "Pets allowed",
-//       "Cooking basics"
-//     ],
-//     "labels": [
-//       "Top of the world",
-//       "Trending",
-//       "Play",
-//       "Tropical"
-//     ],
-//     "host": {
-//       "_id": "u101",
-//       "fullname": "Davit Pok",
-//       "imgUrl": "https://a0.muscache.com/im/pictures/user/User-369818264/original/8fbc6d95-73f9-452a-b427-76a7a4ea4335.jpeg?im_w=240",
-//     },
-//     "loc": {
-//       "country": "Portugal",
-//       "countryCode": "PT",
-//       "city": "Porto",
-//       "address": "17 Kombo st",
-//       "lat": -8.61308,
-//       "lng": 41.1413
-//     },
-//     "reviews": [
-//       {
-//         "id": "10711825",
-//         "at": 1624147574832,
-//         "by": {
-//           "_id": "622f3407e36c59e6164fc004",
-//           "fullname": "Kiesha",
-//           "imgUrl": "https://res.cloudinary.com/dtaiyvzq5/image/upload/v1670705495/kmfst5hn9pq3xax2bxad.webp",
-//         },
-//         "txt": "I had a great experience working with Patty and Beckett.  Both were very attentive in sorting out the booking details and following up directly when I had questions.  I rented a 2 bedroom unit at the Westin Villas  in Maui and both the unit and property was absolutely amazing.  I think we had the best unit on the resort complete with 2 outdoor patios with direct access  to  the  beach.  I would HIGHLY recommend renting with Patty and Beckett.",
-//         "rate": {
-//           "Cleanliness": 4.5,
-//           "Communication": 4.7,
-//           "Check-in": 4.7,
-//           "Accuracy": 4.5,
-//           "Location": 4.9,
-//           "Value": 5.0
-//         }
-//       },
-//       {
-//         "id": "10712425",
-//         "at": 1550016495867,
-//         "by": {
-//           "_id": "622f3607e36c59e6164fc004",
-//           "fullname": "Mary",
-//           "imgUrl": "https://a0.muscache.com/im/pictures/user/d2a27baf-c81d-4258-a8ac-898600a40da1.jpg?im_w=240",
-//         },
-//         "txt": "Beautiful home in a quiet area. The downstairs is really two apartments, one larger and one small for one or two people. It took me some time to figure that out when reading the description. We were only four people but we value space and privacy. A larger group, unless they are a family might find it a bit cramped. The outside area is spacious and location on the water is really great. The outside/pool is shared with the upstairs unit. It was occupied when we were there but without issue. Renting the entire space, both upstairs and downstairs, for a group that know each other well would be ideal. Local restaurants are good, not great. Grocery nearby is convenient. Ierapetra is a 20 minute drive for more expansive services.",
-//         "rate": {
-//           "Cleanliness": 4.8,
-//           "Communication": 5.0,
-//           "Check-in": 4.9,
-//           "Accuracy": 4.9,
-//           "Location": 5.0,
-//           "Value": 5.0
-//         }
-//       },
-//       {
-//         "id": "10678825",
-//         "at": 1478217658374,
-//         "by": {
-//           "_id": "622f3507e36c59e6164fc004",
-//           "fullname": "Sapien",
-//           "imgUrl": "https://a0.muscache.com/im/pictures/user/3c41c6d7-b688-4951-aa83-221571dfbdcb.jpg?im_w=240",
-//         },
-//         "txt": "The best till date",
-//         "rate": {
-//           "Cleanliness": 4.3,
-//           "Communication": 4.6,
-//           "Check-in": 4.9,
-//           "Accuracy": 4.8,
-//           "Location": 4.9,
-//           "Value": 5.0
-//         }
-//       },
-//     ],
-//     "likedByUsers": ['mini-user'] // for user-wishlist : use $in
-//   })
+; (async () => {
+  await storageService.post(STORAGE_KEY, {
+    "_id": "45478gj487dh",
+    "name": "Ribeira Charming Duplex",
+    "type": "House",
+    "imgUrls": ["https://a0.muscache.com/im/pictures/e83e702f-ef49-40fb-8fa0-6512d7e26e9b.jpg?aki_policy=large",
+      "https://a0.muscache.com/im/pictures/e83e702f-ef49-40fb-8fa0-6512d7e26e9b.jpg?aki_policy=large",
+      "https://a0.muscache.com/im/pictures/e83e702f-ef49-40fb-8fa0-6512d7e26e9b.jpg?aki_policy=large",
+      "https://a0.muscache.com/im/pictures/e83e702f-ef49-40fb-8fa0-6512d7e26e9b.jpg?aki_policy=large",
+      "https://a0.muscache.com/im/pictures/e83e702f-ef49-40fb-8fa0-6512d7e26e9b.jpg?aki_policy=large",
+      "https://a0.muscache.com/im/pictures/e83e702f-ef49-40fb-8fa0-6512d7e26e9b.jpg?aki_policy=large"],
+    "price": 80.00,
+    "summary": "Fantastic duplex apartment with three bedrooms, located in the historic area of Porto, Ribeira (Cube)...",
+    "capacity": 8,
+    "amenities": [
+      "TV",
+      "Wifi",
+      "Kitchen",
+      "Smoking allowed",
+      "Pets allowed",
+      "Cooking basics"
+    ],
+    "labels": [
+      "Top of the world",
+      "Trending",
+      "Play",
+      "Tropical"
+    ],
+    "host": {
+      "_id": "u101",
+      "fullname": "Davit Pok",
+      "imgUrl": "https://a0.muscache.com/im/pictures/user/User-369818264/original/8fbc6d95-73f9-452a-b427-76a7a4ea4335.jpeg?im_w=240",
+    },
+    "loc": {
+      "country": "Portugal",
+      "countryCode": "PT",
+      "city": "Porto",
+      "address": "17 Kombo st",
+      "lat": -8.61308,
+      "lng": 41.1413
+    },
+    "reviews": [
+      {
+        "id": "10711825",
+        "at": 1624147574832,
+        "by": {
+          "_id": "622f3407e36c59e6164fc004",
+          "fullname": "Kiesha",
+          "imgUrl": "https://res.cloudinary.com/dtaiyvzq5/image/upload/v1670705495/kmfst5hn9pq3xax2bxad.webp",
+        },
+        "txt": "I had a great experience working with Patty and Beckett.  Both were very attentive in sorting out the booking details and following up directly when I had questions.  I rented a 2 bedroom unit at the Westin Villas  in Maui and both the unit and property was absolutely amazing.  I think we had the best unit on the resort complete with 2 outdoor patios with direct access  to  the  beach.  I would HIGHLY recommend renting with Patty and Beckett.",
+        "rate": {
+          "Cleanliness": 4.5,
+          "Communication": 4.7,
+          "Check-in": 4.7,
+          "Accuracy": 4.5,
+          "Location": 4.9,
+          "Value": 5.0
+        }
+      },
+      {
+        "id": "10712425",
+        "at": 1550016495867,
+        "by": {
+          "_id": "622f3607e36c59e6164fc004",
+          "fullname": "Mary",
+          "imgUrl": "https://a0.muscache.com/im/pictures/user/d2a27baf-c81d-4258-a8ac-898600a40da1.jpg?im_w=240",
+        },
+        "txt": "Beautiful home in a quiet area. The downstairs is really two apartments, one larger and one small for one or two people. It took me some time to figure that out when reading the description. We were only four people but we value space and privacy. A larger group, unless they are a family might find it a bit cramped. The outside area is spacious and location on the water is really great. The outside/pool is shared with the upstairs unit. It was occupied when we were there but without issue. Renting the entire space, both upstairs and downstairs, for a group that know each other well would be ideal. Local restaurants are good, not great. Grocery nearby is convenient. Ierapetra is a 20 minute drive for more expansive services.",
+        "rate": {
+          "Cleanliness": 4.8,
+          "Communication": 5.0,
+          "Check-in": 4.9,
+          "Accuracy": 4.9,
+          "Location": 5.0,
+          "Value": 5.0
+        }
+      },
+      {
+        "id": "10678825",
+        "at": 1478217658374,
+        "by": {
+          "_id": "622f3507e36c59e6164fc004",
+          "fullname": "Sapien",
+          "imgUrl": "https://a0.muscache.com/im/pictures/user/3c41c6d7-b688-4951-aa83-221571dfbdcb.jpg?im_w=240",
+        },
+        "txt": "The best till date",
+        "rate": {
+          "Cleanliness": 4.3,
+          "Communication": 4.6,
+          "Check-in": 4.9,
+          "Accuracy": 4.8,
+          "Location": 4.9,
+          "Value": 5.0
+        }
+      },
+    ],
+    "likedByUsers": ['mini-user'] // for user-wishlist : use $in
+  })
 
-// })()
+})()
