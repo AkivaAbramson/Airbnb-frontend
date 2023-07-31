1dc0d07f6013e575636d0160c4a19fed0e52d020
Akiva Abramson <akivaabramson@gmail.com>
2023-08-01 00:25:45 +0300

    add search bar Modals

---
+++ src/cmps/Destinations.vue
@@ -0,0 +1,40 @@
+<template>
+    <section class="dest-container">
+        <h1 class="dest-title">Search by region</h1>
+        <div class="map-container">
+            <div class="map-preview">
+                <img class="map-img" alt="" src="https://a0.muscache.com/pictures/f9ec8a23-ed44-420b-83e5-10ff1f071a13.jpg">
+                <h5>I'm flexible</h5>
+            </div>
+            <div class="map-preview">
+                <img class="map-img" alt="" src="https://a0.muscache.com/im/pictures/66355b01-4695-4db9-b292-c149c46fb1ca.jpg?im_w=320">
+                <h5>Middle East</h5>
+            </div>
+            <div class="map-preview">
+                <img class="map-img" alt="" src="https://a0.muscache.com/im/pictures/ea5598d7-2b07-4ed7-84da-d1eabd9f2714.jpg?im_w=320">
+                <h5>Italy</h5>
+            </div>
+            <div class="map-preview">
+                <img class="map-img" alt="" src="https://a0.muscache.com/im/pictures/4e762891-75a3-4fe1-b73a-cd7e673ba915.jpg?im_w=320">
+                <h5>United States</h5>
+            </div>
+            <div class="map-preview">
+                <img class="map-img" alt="" src="https://a0.muscache.com/im/pictures/09be1400-6a42-4a4f-90f6-897e50110031.jpg?im_w=320">
+                <h5>Greece</h5>
+            </div>
+            <div class="map-preview">
+                <img class="map-img" alt="" src="https://a0.muscache.com/im/pictures/06a30699-aead-492e-ad08-33ec0b383399.jpg?im_w=320">
+                <h5>South America</h5>
+            </div>
+        </div>
+        
+    </section>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="">
+    
+</style>
